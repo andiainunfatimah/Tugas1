fcefc801a1e80d7e9fa3c4a045ec8301734f7b59
andiainunfatimah <127599358+andiainunfatimah@users.noreply.github.com>
2023-03-11 18:32:16 +0700

    Create Asgdll.java

---
+++ Tugas1_SourceCode_Praktek/Asgdll.java
@@ -0,0 +1,16 @@
+/*NIM                       : 13020210084
+  Nama                      : Andi Ainun Fatimah Jamal
+  Hari/Tanggal              : Kamis, 09 Maret 2023
+  Waktu Pengerjaan          : 09.56 */
+
+public class Asgdll {
+
+	public static void main (String [] args) {
+	float f= 20.0f;
+	double fll;
+	fll=10.0f;
+	System.out.println ("f : "+f+ "\nfll: "+fll);
+
+	}
+
+}
